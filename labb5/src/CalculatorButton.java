5c5098625a191de1b720e7d56b21e78c76513a26
malco <56635871+salon64@users.noreply.github.com>
2023-02-20 14:12:37 +0100

    removed test

--- labb5/src/CalculatorButton.java
+++ labb5/src/CalculatorButton.java
@@ -4,4 +4,6 @@ public abstract class CalculatorButton extends JButton {
     public CalculatorButton(String label, Situation situation) {
         super(label);
     }
+
+    public abstract void transition(Situation situation);
 }
