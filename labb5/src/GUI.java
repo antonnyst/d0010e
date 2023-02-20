d9a85289bdb3cb7d297547fa572c06e5901a386d
antonnyst <35500074+antonnyst@users.noreply.github.com>
2023-02-20 12:52:16 +0100

    Start GUI

--- labb5/src/GUI.java
+++ labb5/src/GUI.java
@@ -1,16 +1,45 @@
+import java.awt.Color;
+import java.awt.GridBagLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import java.awt.GridBagLayout;  
+
 
 public class GUI extends JFrame {
     
     JPanel canvas;
+    JLabel display;
+    JPanel keyPad;
+    
 
     public GUI() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         this.canvas = new JPanel();
-        this.setContentPane(canvas);
-        
         this.canvas.setLayout(new GridBagLayout());
+
+        // Create display
+        this.display = new JLabel();
+        this.display.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        this.display.setText("TEST");
+
+        // Create keyPad
+
+
+
+        this.keyPad = new JPanel();
+        
+        
+        // Add components to canvas
+        this.canvas.add(this.display);
+        this.canvas.add(this.keyPad);
+
+
+
+
+        this.setContentPane(canvas);
         
         this.pack();
         this.setVisible(true);
