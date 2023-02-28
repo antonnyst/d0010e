d0b303890a55b40f1c9044d6ed91cb9fd2d2fd85
antonnyst <35500074+antonnyst@users.noreply.github.com>
2023-02-28 10:42:32 +0100

    PickupTime

--- labb6/src/snabbköp/PickupTime.java
+++ labb6/src/snabbköp/PickupTime.java
@@ -1,5 +1,19 @@
 package snabbköp;
 
+import java.util.Random;
+
 public class PickupTime {
+    private Random random;
+    private double min;
+    private double max;
     
+    public PickupTime(long f, double min, double max) {
+        this.random = new Random(f);
+        this.min = min;
+        this.max = max;
+    }
+
+    public double finishTime(double currentTime) {
+        return currentTime + random.nextDouble(min, max);
+    }
 }
