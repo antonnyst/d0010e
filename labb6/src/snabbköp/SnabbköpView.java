adab09600c065f53401937b35f031fa6b35d2d9f
antonnyst <35500074+antonnyst@users.noreply.github.com>
2023-02-28 10:35:12 +0100

    SnabbköpView fix

--- labb6/src/snabbköp/SnabbköpView.java
+++ labb6/src/snabbköp/SnabbköpView.java
@@ -1,9 +1,17 @@
 package snabbköp;
 
+import java.util.Observable;
+import allmänt.State;
+import allmänt.View;
+
 public class SnabbköpView extends View {
     
-    @Override
-    public update() {
+    public SnabbköpView(State state) {
+        super(state);
+    }
 
+    @Override
+    public void update(Observable arg0, Object arg1) {
+        // TODO gör en utskrift
     }
 }
