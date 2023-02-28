e71d4ef49b981ec3d88be01e82e5b013294d6a11
antonnyst <35500074+antonnyst@users.noreply.github.com>
2023-02-28 10:41:58 +0100

    PaymentTime

--- labb6/src/snabbköp/PaymentTime.java
+++ labb6/src/snabbköp/PaymentTime.java
@@ -1,5 +1,19 @@
 package snabbköp;
 
+import java.util.Random;
+
 public class PaymentTime {
+    private Random random;
+    private double min;
+    private double max;
     
+    public PaymentTime(long f, double min, double max) {
+        this.random = new Random(f);
+        this.min = min;
+        this.max = max;
+    }
+
+    public double finishTime(double currentTime) {
+        return currentTime + random.nextDouble(min, max);
+    }
 }
