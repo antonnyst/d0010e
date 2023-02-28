28995b6a68f87430e8b2b5caeac628ae78ebc8d1
antonnyst <35500074+antonnyst@users.noreply.github.com>
2023-02-28 10:55:01 +0100

    ArrivalTime

--- labb6/src/snabbköp/ArrivalTime.java
+++ labb6/src/snabbköp/ArrivalTime.java
@@ -1,5 +1,18 @@
 package snabbköp;
 
+import java.util.Random;
+
 public class ArrivalTime {
+    private Random random;
+    private double lambda;
     
+    public ArrivalTime(long f, double lambda) {
+        this.random = new Random(f);
+        this.lambda = lambda;
+    }
+
+    public double finishTime(double currentTime) {
+        // Exponentialdistrubering med lambda
+        return currentTime + (Math.log(1-random.nextDouble())/(-lambda));
+    }
 }
