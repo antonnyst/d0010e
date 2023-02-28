f84f5426b1272b09b1c90b2d1f1f7df0850d1ba2
antonnyst <35500074+antonnyst@users.noreply.github.com>
2023-02-28 15:38:16 +0100

    AAAA

--- labb6/src/snabbköp/snabbköpsEvent/ArrivalEvent.java
+++ labb6/src/snabbköp/snabbköpsEvent/ArrivalEvent.java
@@ -3,10 +3,16 @@ package snabbköp.snabbköpsEvent;
 import allmänt.Event;
 import allmänt.EventQueue;
 import snabbköp.SnabbköpState;
+import snabbköp.CustomerFactory.Customer;
 
 public class ArrivalEvent extends Event {
-    public ArrivalEvent(SnabbköpState state, EventQueue queue, double time) {
+
+    private Customer customer;
+
+    public ArrivalEvent(SnabbköpState state, EventQueue queue, double time, Customer customer) {
         super(state, queue, time);
+        this.customer = customer;
+
     }
 
     @Override
@@ -18,7 +24,12 @@ public class ArrivalEvent extends Event {
             
             // Lägg till nästa arrival
             double arrivalTime = ((SnabbköpState)this.state).getArrivalTime().finishTime(time);
-            ArrivalEvent arrivalEvent = new ArrivalEvent((SnabbköpState)state, queue, arrivalTime);
+            ArrivalEvent arrivalEvent = new ArrivalEvent(
+                (SnabbköpState)state,
+                queue,
+                arrivalTime,
+                ((SnabbköpState)this.state).getCustomerFactory().getCustomer()
+                );
             this.queue.insert(arrivalEvent);
 
             if (((SnabbköpState)this.state).getAntalKunder() < ((SnabbköpState)this.state).getMaxAntalKunder()) {
@@ -29,7 +40,7 @@ public class ArrivalEvent extends Event {
                 
                 // Lägg till pickupevent för när vi plockat klart
                 double pickupTime = ((SnabbköpState)this.state).getPickupTime().finishTime(time);
-                PickupEvent pickupEvent = new PickupEvent(state, queue, pickupTime);
+                PickupEvent pickupEvent = new PickupEvent((SnabbköpState)state, queue, pickupTime);
                 this.queue.insert(pickupEvent);
             } else {
                 // Missad kund
--- labb6/src/snabbköp/snabbköpsEvent/PickupEvent.java
+++ labb6/src/snabbköp/snabbköpsEvent/PickupEvent.java
@@ -2,15 +2,33 @@ package snabbköp.snabbköpsEvent;
 
 import allmänt.Event;
 import allmänt.EventQueue;
-import allmänt.State;
+import snabbköp.SnabbköpState;
+import snabbköp.CustomerFactory.Customer;
 
 public class PickupEvent extends Event {
-    public PickupEvent(State state, EventQueue queue, double time) {
+    
+    private Customer customer;
+    private SnabbköpState state;
+
+    public PickupEvent(SnabbköpState state, EventQueue queue, double time, Customer customer) {
         super(state, queue, time);
+        this.customer = customer;
     }
 
     @Override
     public void runEvent() {
         super.runEvent();
+
+        if (this.state.getLedigaKassor() > 0) {
+            // Vi betalar direkt
+            this.state.decreaseLedigaKassor();
+
+            // Skapa paymentevent för när vi betalat klart
+            double paymentTime = ((SnabbköpState)this.state).getPaymentTime().finishTime(this.time);
+            PaymentEvent paymentEvent = new PaymentEvent(state, queue, paymentTime);
+            this.queue.insert(paymentEvent);
+        } else {
+            ((SnabbköpState)this.state).getKassakö().add(this.customer);
+        }
     }
 }
