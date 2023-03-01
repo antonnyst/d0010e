d77a44faa48ac8095a8cb141fbd4a881a160fe33
antonnyst <35500074+antonnyst@users.noreply.github.com>
2023-03-01 15:50:45 +0100

    Timecalc

--- labb6/src/allmänt/State.java
+++ labb6/src/allmänt/State.java
@@ -4,13 +4,28 @@ import java.util.Observable;
 
 public class State extends Observable {
     public boolean stop;
+    private double time;
 
     public State() {
         stop = false;
     }
 
     public void notify(Event source) {
+        // Spara när eventet hände
+        this.setTime(source.getTime());
+
         this.setChanged();
         this.notifyObservers(source);
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        if (this.time > time) {
+            throw new RuntimeException("Can not decrease time");
+        }
+        this.time = time;
     }
 }
