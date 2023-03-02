b6d125428cf51913a4524803bbef75084438fa4a
malco <56635871+salon64@users.noreply.github.com>
2023-03-02 14:19:02 +0100

    metod 1 och 2 i Optimize

--- labb6/src/huvudprogram/Optimize.java
+++ labb6/src/huvudprogram/Optimize.java
@@ -1,5 +1,40 @@
 package huvudprogram;
 
+import allmänt.EventQueue;
+import allmänt.Simulator;
+import allmänt.StopEvent;
+import snabbköp.SnabbköpState;
+import snabbköp.SnabbköpView;
+import snabbköp.snabbköpsEvent.CloseStoreEvent;
+import snabbköp.snabbköpsEvent.SnabbköpStartEvent;
+
 public class Optimize {
+    
+
+
+
+
+    public SnabbköpState runOptSim(int maxKunder,int antalKassor, Double lambda,Double kmin,Double kmax,double pmin, double pmax, int f){
+        SnabbköpState state = new SnabbköpState(maxKunder,antalKassor,lambda,kmin,kmax,pmin,pmax,f);
+        EventQueue queue = new EventQueue();
+
+        queue.insert(new SnabbköpStartEvent(state, queue));
+        queue.insert(new StopEvent(state, queue, 999));
+        queue.insert(new CloseStoreEvent(state, queue, 10));
+        
+        Simulator sim = new Simulator(state, queue);
+        sim.run();
+        return state;
+    }
+
+    public int findOpt(int maxKunder, Double lambda,Double kmin,Double kmax,double pmin, double pmax, int f){
+        for (int i = 1; i <= maxKunder; i++){
+            if (runOptSim(maxKunder,i,lambda,kmin,kmax,pmin,pmax,f).getAntalKunderMissat() == 0){
+                return i;
+            }
+        }
+        return maxKunder;
+    }
+
     
 }
