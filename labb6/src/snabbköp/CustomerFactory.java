e37a88231b0b42c68b2ea18f9ee79a0ca5333178
antonnyst <35500074+antonnyst@users.noreply.github.com>
2023-03-08 16:02:15 +0100

    CustomerFactory

--- labb6/src/snabbköp/CustomerFactory.java
+++ labb6/src/snabbköp/CustomerFactory.java
@@ -6,20 +6,33 @@
  */
 package snabbköp;
 
+/**
+* Fabrik för att att skapa kunder med unika id.
+*/
 public class CustomerFactory {
     private int nextId;
 
+    /**
+    * Intern klass för kunder som lagrar en int som id
+    */
     public class Customer {
         public final int id;
         private Customer(int id) {
             this.id = id;
         }
     }
-    
+
+    /**
+    * Konstruktor för CustomerFactory.
+    */
     public CustomerFactory() {
         nextId = 0;
     }
 
+    /**
+    * Returnerar nästa kund 
+    * @return Nästa kund som en Customer
+    */
     public Customer getCustomer() {
         Customer customer = new Customer(nextId);
         nextId++;
